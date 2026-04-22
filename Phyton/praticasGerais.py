4e5cf6a0bffa2db8077604a53d0337434770e8e7
MateusDallaz <mdallaz667@gmail.com>
2026-04-22 14:23:00 -0300

    Phyaton

--- Phyton/praticasGerais.py
+++ Phyton/praticasGerais.py
@@ -21,3 +21,64 @@ for i in range (n, -1, -1):
 
 #Exercicio 4
 
+contador = 1
+soma = 0
+
+while contador <= 5:
+    numero = int(input('Digite um numero: '))
+    contador +=1
+    soma += numero
+    
+print(f'A soma dos números é {soma}')
+
+#Exercicio 5: 
+
+nomes = []
+contador = 1
+
+while contador <= 5:
+    nome = input(f'Digite o {contador}° nome: ')
+    nomes.append(nome)
+    contador += 1
+
+print('\nLista de Nomes:')
+for i, nome in enumerate(nomes, start = 1):
+    print(f'{i} - {nome}')
+    
+
+#Exercicio 6:
+
+temperaturas = []
+contador = 1
+
+while contador <= 7:
+    dias = float(input(f'Digite a temperatura registrada no {contador}° dia: '))
+    temperaturas.append(dias)
+    contador += 1
+    
+
+media = sum(temperaturas)/len(temperaturas)
+
+print(f'\nA temperatura máxima registrada na semana foi {max(temperaturas)}°C, a minima foi {min(temperaturas)}°C e a média semanal foi {media:.2f}°C')
+
+#Exercicio 7:
+
+nome = None
+
+def saudacoes():
+    nome = input('Digite seu nome: ')
+    print(f'Saudações {nome}, Seja bem vindo')
+
+saudacoes()
+
+#Exercico 8:
+
+idade = int(input('Digite sua idade: '))
+
+def verificarIdade():
+    if idade >= 18:
+        print('Você é maior de idade')
+    else: 
+        print('Você é menor de idade')
+        
+verificarIdade()
