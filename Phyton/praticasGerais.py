6ff4acc52190d2fdadda50d716bbe0cf615df3f2
MateusDallaz <mdallaz667@gmail.com>
2026-04-22 12:40:26 -0300

    Phyaton

---
+++ Phyton/praticasGerais.py
@@ -0,0 +1,23 @@
+#Exercico 1:
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: ? '))
+profissao = input('Qual sua profissão: ')
+
+print(f'Olá, meu nome é {nome}, tenho {idade} anos e sou {profissao}')
+
+#Exercicio 2:
+
+n = int(input('Digite um número: '))
+
+print(f'O antecessor de {n} é {n-1}, e o seu sucessor é {n+1}')
+
+#Exercicio 3: 
+
+n = int(input('Digite um numero: '))
+
+for i in range (n, -1, -1):
+    print(i)
+
+#Exercicio 4
+
